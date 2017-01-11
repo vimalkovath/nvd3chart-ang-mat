48375ffa22f388cbe1966e209d5512fd8b87f973
Vimal K V <vimal.v@marlabs.com>
2017-01-11 12:33:26 +0530

    bar discreate real data

--- src/app/controllers/BarDiscreate.js
+++ src/app/controllers/BarDiscreate.js
@@ -1,110 +1,117 @@
-(function () {
+(function() {
     angular
         .module('app')
-        .controller('BarDiscreateController', [
-        'tableService',
+        .controller('BarDiscreateController', ['sharedDataService',
+            'tableService',
             BarDiscreateController
         ]);
 
-    function BarDiscreateController(tableService) {
+    function BarDiscreateController(sharedDataService, tableService) {
         var vm = this;
+        //Each bar represents a single discrete quantity.
+        vm.discreateBarData = [{
+            key: "Spend by Tendur in Months",
+            values: [{
+                    "label": "Less than 10",
+                    "value": 0
+                },
+                {
+                    "label": "10 to 15",
+                    "value": 0
+                },
+                {
+                    "label": "15 to 20",
+                    "value": 0
+                },
+                {
+                    "label": "20 to 25",
+                    "value": 0
+                },
+                {
+                    "label": "Above 25",
+                    "value": 0
+                }
+            ]
+        }];
+
+        console.log("data discreate" + vm.discreateBarData[0].values[0].value);
 
 
- vm.barDiscreateServiceReturn = tableService.loadDiscreateBarValue();
-   
-   var discreateBarData=[];
-   discreateBarData.push({key:"vimal",values:vm.barDiscreateServiceReturn});
-   
-   vm.discreateBarData=discreateBarData;
-   console.log(vm.discreateBarData);
-   
+        /***  getting data from server/csv and manipulate with chart */
+        //   vm.getChartData = function() {
+        sharedDataService.loadAllItemsFromCsv()
+            .then(function(chartData) {
+                vm.chartData = chartData;
+                console.log("bar chartGetting data" + chartData);
 
-//Each bar represents a single discrete quantity.
- vm.data = [
-            {
-                key: "Cumulative Return",
-                values: [
-                    {
-                        "label" : "A" ,
-                        "value" : 29.765957771107
-                    } ,
-                    {
-                        "label" : "B" ,
-                        "value" : 0
-                    } ,
-                    {
-                        "label" : "C" ,
-                        "value" : 32.807804682612
-                    } ,
-                    {
-                        "label" : "D" ,
-                        "value" : 196.45946739256
-                    } ,
-                    {
-                        "label" : "E" ,
-                        "value" : 23.19434030906893
-                    } ,
-                    {
-                        "label" : "F" ,
-                        "value" : 98.079782601442
-                    } ,
-                    {
-                        "label" : "G" ,
-                        "value" : 18.079782601442
-                    } ,
-                    {
-                        "label" : "H" ,
-                        "value" : 98.079782601442
-                    } ,
-                    {
-                        "label" : "I" ,
-                        "value" : 98.079782601442
+
+                var count = 0;
+                var data = chartData.data;
+
+                vm.calculateSum = function(data) {
+                    for (var i = 0; i < data.length; i++) {
+                        var Tenure_Range = data[i].Tenure_Range;
+                        console.log("Tenure_Range" + Tenure_Range);
+                        if (Tenure_Range == " Less than 10 ") {
+                            vm.discreateBarData[0].values[0].value = vm.discreateBarData[0].values[0].value + 2;
+                        } //don't forget to add the base 
+                        if (Tenure_Range == " 10 to 15 ") {
+                            vm.discreateBarData[0].values[1].value = vm.discreateBarData[0].values[1].value + 1;
+                        }
+                        if (Tenure_Range == " 15 to 20 ") {
+                            vm.discreateBarData[0].values[2].value = vm.discreateBarData[0].values[2].value + 1;
+                        }
+                        if (Tenure_Range == " 20 to 25 ") {
+                            vm.discreateBarData[0].values[3].value = vm.discreateBarData[0].values[3].value + 1;
+                        } //don't forget to add the base 
+                        if (Tenure_Range == " Above25 ") {
+                            vm.discreateBarData[0].values[4].value = vm.discreateBarData[0].values[4].value + 1;
+                        }
+                        console.log(data[i].Tenure_Range + "Tender count ");
                     }
-                ]
-            }
-        ];
-       
-console.log(vm.data);
+                }
+                vm.calculateSum(data);
+            });
+        //   };
 
-       
-        // TODO: move data to the service
+        //   vm.barDiscreateServiceReturn = tableService.loadDiscreateBarValue();
+        var discreateBarData = [];
+        //  discreateBarData.push({ key: "vimal", values: vm.barDiscreateServiceReturn });
 
-vm.options = {
-  
-chart: {
+        //  vm.discreateBarData = discreateBarData;
+        console.log(vm.discreateBarData);
+
+
+
+
+        // TODO: move data to the service krispo
+
+        vm.options = {
+            chart: {
                 type: 'discreteBarChart',
                 height: 350,
-                x: function(d){return d.label;},
-                y: function(d){return d.value;},
+                x: function(d) { return d.label; },
+                y: function(d) { return d.value; },
                 showValues: true,
-                valueFormat: function(d){ return d3.format(',.4f')(d); },
+                valueFormat: function(d) { return d3.format(',.4f')(d); },
                 dispatch: {
-                  tooltipShow: function(e){ console.log('! tooltip SHOW !')},
-                  tooltipHide: function(e){console.log('! tooltip HIDE !')},
-                  beforeUpdate: function(e){ console.log('! before UPDATE !')}
+                    tooltipShow: function(e) { console.log('! tooltip SHOW !') },
+                    tooltipHide: function(e) { console.log('! tooltip HIDE !') },
+                    beforeUpdate: function(e) { console.log('! before UPDATE !') }
                 },
                 discretebar: {
-                  dispatch: {
-                    elementClick: function(e) {
-                    var e=e;
-                        console.log("! element Click !"+e);
-                    },
-                    elementDblClick: function(e) {console.log("! element Double Click !")}
-                  }
+                    dispatch: {
+                        elementClick: function(e) {
+                            var e = e;
+                            console.log("! element Click !" + e);
+                        },
+                        elementDblClick: function(e) { console.log("! element Double Click !") }
+                    }
                 },
-                callback: function(e){console.log('! callback !')}
+                callback: function(e) { console.log('! callback !') }
             }
 
-};
-
-
-
-
-
-
-
-
-
+        };
 
 
     }
--- src/app/controllers/PieController.js
+++ src/app/controllers/PieController.js
@@ -1,21 +1,75 @@
-(function () {
+(function() {
     angular
         .module('app')
-        .controller('PieController', [
+        .controller('PieController', ['sharedDataService',
             PieController
         ]);
 
-    function PieController() {
+    function PieController(sharedDataService) {
         var vm = this;
 
+        vm.data = [{
+                key: "Male",
+                y: 0
+            },
+            {
+                key: "Female",
+                y: 0
+            },
+            {
+                key: "Others",
+                y: 0
+            }
+
+        ];
+
+        // vm.calculateX = function() {
+        sharedDataService.loadAllItemsFromCsv()
+            .then(function(chartData) {
+                // var obj = JSON.parse(chartData);
+                // vm.chartData = obj;
+                vm.chartData = chartData;
+                var count = 0;
+                var data = chartData.data;
+
+                vm.calculateSum = function(data) {
+                        for (var i = 0; i < data.length; i++) {
+                            var sex = data[i].SEX;
+                            if (sex == 1) {
+                                vm.data[0].y = vm.data[0].y + 1;
+                            } //don't forget to add the base 
+                            if (sex == 2) {
+                                count = count + 1;
+                                console.log(count)
+                                vm.data[1].y = vm.data[1].y + 1;
+                            }
+                            if (sex == 3) {
+                                count = count + 1;
+                                console.log(count)
+                                vm.data[2].y = vm.data[2].y + 1;
+                            }
+                            console.log(data[i].SEX + "gender count " + count);
+                        }
+                    }
+                    // console.log(count);
+                console.log("Getting chartData" + chartData.data.length);
+
+                vm.calculateSum(data);
 
 
-  vm.options = {
+
+            });
+
+        //   }
+
+
+
+        vm.options = {
             chart: {
                 type: 'pieChart',
                 height: 300,
-                x: function(d){return d.key;},
-                y: function(d){return d.y;},
+                x: function(d) { return d.key; },
+                y: function(d) { return d.y; },
                 showLabels: true,
                 duration: 500,
                 labelThreshold: 0.01,
@@ -27,40 +81,27 @@
                         bottom: 5,
                         left: 0
                     }
-                }
+                },
+                //chart events
+                pie: {
+                    dispatch: {
+                        // chartClick: function(e) {
+                        //     console.log("clicked");
+                        // },
+                        elementClick: function(e) {
+                            var a;;
+                            console.log("clicked" + e.data.key)
+                        }
+
+                    }
+                },
             }
         };
 
-        vm.data = [
-            {
-                key: "One",
-                y: 5
-            },
-            {
-                key: "Two",
-                y: 2
-            },
-            {
-                key: "Three",
-                y: 9
-            },
-            {
-                key: "Four",
-                y: 7
-            },
-            {
-                key: "Five",
-                y: 4
-            },
-            {
-                key: "Six",
-                y: 3
-            },
-            {
-                key: "Seven",
-                y: .5
-            }
-        ];
+
+
+
+
 
 
 
